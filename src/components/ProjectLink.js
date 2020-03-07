bb9cc4f5f5c3ccdf997123b732b098eaf86aaa48
Jonathan Ho <johnnyho82@gmail.com>
2020-03-07 10:48:11 -0500

    Fix Links hover styles

--- src/components/ProjectLink.js
+++ src/components/ProjectLink.js
@@ -5,7 +5,7 @@ export default function ProjectLink({ link }) {
 	return (
 		<div className='project-link'>
 			<a href={link.url} target='_blank' rel='noopener noreferrer'>
-				<FontAwesomeIcon icon={link.icon} className='icon' size='lg' />{' '}
+				<FontAwesomeIcon icon={link.icon} className='link-icon' size='lg' />{' '}
 				{link.text}
 			</a>
 		</div>
