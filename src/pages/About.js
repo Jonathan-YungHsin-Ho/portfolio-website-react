7eeea689cd6237674d6341f8fa1958a0868ba038
Jonathan Ho <johnnyho82@gmail.com>
2020-03-08 19:11:33 -0400

    Update About Me text

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,13 +8,12 @@ export default function About() {
 				<div className='about-text'>
 					{/* <h2>About Me</h2> */}
 					<p>
-						After over a decade working with students of all ages in both public
-						and independent school settings, I decided to embrace a new
-						challenge and expand my skills in a different direction. I joined
-						the field of computer programming, particularly web development,
-						because I thoroughly enjoy flexing the logical reasoning and
-						problem-solving muscles required to bring a coding project to
-						completion.
+						After over a decade working with students of all ages in public and
+						independent school settings, I decided to embrace a new challenge
+						and expand my skills in a different direction. I joined the field of
+						computer programming, particularly web development, because I
+						thoroughly enjoy flexing the logical reasoning and problem-solving
+						muscles required to bring a coding project to completion.
 					</p>
 					<br />
 					<p>
@@ -22,7 +21,7 @@ export default function About() {
 						knowledge and seeking resources to address any gaps in my current
 						understanding. Additionally, the communication and collaboration
 						skills I've developed over my career as an educator, as well as the
-						flexibility and adaptability that comes from working with diverse
+						flexibility and adaptability that come from working with diverse
 						groups of people, including my students and other educators, are
 						strengths that I bring with me to all my pursuits. I'm always
 						learning and growing, and I'm excited to apply all that I've done
